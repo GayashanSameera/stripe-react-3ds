64895be7e62ed00c603a58b9b109a0923914f70a
GayashanSameera <gayashansameera30@gmail.com>
2023-12-01 21:43:32 +0530

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import Checkout from "./Checkout";
 
-const stripePromise = loadStripe("pk_test_51F3Np0D9QG4YLeVKJCMGn0SjhWgLP2NeLz1zLydNlRRKez5L38lsyWZLaEDsOmklJCJ6TrLsOO9IweH6sTsiqtZG00XufRxPlT");
+const stripePromise = loadStripe("");
 
 function App() {
   return (
